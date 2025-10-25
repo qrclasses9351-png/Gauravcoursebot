0792927f4a4605ae9b5dee67a54dc00a95f02b7e
qrclasses9351-png <qrclasses9351@gmail.com>
2025-10-25 22:26:36 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,14 @@
+# Don't Remove Credit Tg - @ALL_CORSE_H
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+from os import environ
+
+API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+
+
+
+
+
